81f3cd447e5c02851d5f56652e961492698a59ad
Christopher Dougherty <christophertdoc@gmail>
2018-09-11 08:44:11 -0400

    refactor function location

--- src/components/FundingRounds.js
+++ src/components/FundingRounds.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import Content from '../content/funding_rounds';
 
-let users = [
-    {name: "John", id: 120, age: 22, gender: "male"},
-    {name: "Beth", id: 443, age: 24, gender: "female"},
-    {name: "Jane", id: 510, age: 19, gender: "female"}
-]
 
 class FundingRounds extends React.Component {
 
 
-
     render() {
+
+        const listItems = Content.map((company) =>
+            <li>{company.company_name}: {company.raised_amount_usd}</li>
+        );
+
         return(
             <div>
-                {Content.map(company => {
-                    return (
-                        <li>{company.company_name}: {company.raised_amount_usd}</li>
-                    );
-                })}
+                {listItems}
             </div>
         )
     }
 }
 
 export default FundingRounds;
+
+// {Content.map(company => {
+//     return (
+//         <li>{company.company_name}: {company.raised_amount_usd}</li>
+//     );
+// })}
