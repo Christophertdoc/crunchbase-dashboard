a39504652b66d026d1df0c809c75c4458bee9ef6
Christopher Dougherty <christophertdoc@gmail>
2018-09-12 06:45:36 -0400

    map

--- src/components/FundingRounds.js
+++ src/components/FundingRounds.js
@@ -7,13 +7,41 @@ class FundingRounds extends React.Component {
 
     render() {
 
-        const listItems = Content.map((company) =>
-            <li>{company.raised_amount_usd}</li>
+        const listItems = Content.map((company, index) =>
+            company.raised_amount_usd
         );
+
+        const sorted = listItems.sort(function(a, b){
+            return <li>{a - b}</li>
+        })
+
+        var list = [2, 1, 5, 4];
+
+        // temporary array holds objects with position and sort-value
+        var mapped = list.map(function(el, i) {
+            return { index: i, value: el };
+        })
+        
+        // sorting the mapped array containing the reduced values
+        mapped.sort(function(a, b) {
+            if (a.value > b.value) {
+            return 1;
+            }
+            if (a.value < b.value) {
+            return -1;
+            }
+            return 0;
+        });
+        
+        // container for the resulting order
+        var result = mapped.map(function(el){
+            return list[el.index];
+        });
 
         return(
             <div>
-                {listItems}
+                {result}
+               
             </div>
         )
     }
@@ -21,5 +49,4 @@ class FundingRounds extends React.Component {
 
 export default FundingRounds;
 
-// How to create a new array with map https://codeburst.io/learn-understand-javascripts-map-function-ffc059264783
-// https://stackoverflow.com/questions/12491101/javascript-create-array-from-for-loop 
+// Sorting with map: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Sorting_with_map 
