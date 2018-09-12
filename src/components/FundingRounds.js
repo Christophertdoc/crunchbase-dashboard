79b494feac65970e5d405025a2b1081606139510
Christopher Dougherty <christophertdoc@gmail>
2018-09-12 10:43:29 -0400

    list attempts with notes

--- src/components/FundingRounds.js
+++ src/components/FundingRounds.js
@@ -7,21 +7,54 @@ class FundingRounds extends React.Component {
 
     render() {
 
-        const listItems = Content.map((company, index) =>
-            company.raised_amount_usd
-        );
 
-        const sorted = listItems.sort(function(a, b){
-            return <li>{a - b}</li>
-        })
+        // RETURNS RAISED AMOUNT BUT NOT IN AN ARRAY OR OBJECT
+        // const listItems = Content.map((company, index) =>
+        //     company.raised_amount_usd
+        // );
 
+        // SORTS ELEMENTS BUT NOT BASED ON NUMBERS IN ASCENDING ORDER
+        // const sorted = listItems.sort(function(a, b){
+        //     return <li>{a - b}</li>
+        // })
+
+
+
+        // RETURNS A BROWSER ERROR SAYING SOMETHING ABOUT REACT NOT BEING 
+        // ABLE TO RETURN OBJECT AS A CHILD ELEMENT
+        // var list = [{"key": "1"},{"key": "3"},{"key": "2"},{"key": "5"}];
+        // // temporary array holds objects with position and sort-value
+        // const mapped = list.map((company, index) =>
+        //     // return { index: i, value: el.key };
+        //     return { company.key };
+        // );
+        // var mapped = list.map(function(el, i) {
+        //     return { index: i, value: el.key };
+        //   })
+        // // sorting the mapped array containing the reduced values
+        // mapped.sort(function(a, b) {
+        //     if (a.value > b.value) {
+        //     return 1;
+        //     }
+        //     if (a.value < b.value) {
+        //     return -1;
+        //     }
+        //     return 0;
+        // });
+        // // container for the resulting order
+        // var result = mapped.map(function(el){
+        //     return list[el.value];
+        // });
+
+
+
+        // WORKS IN THAT IT SORTS NUMBERS ON ARRAY IN ASCENDING ORDER 
+        // CAN'T SEEM TO GET THIS TO WORK ON THE JSON OBJECT AS SHOWN IN ANOTHER ATTEMPT
         var list = [2, 1, 5, 4];
-
         // temporary array holds objects with position and sort-value
         var mapped = list.map(function(el, i) {
             return { index: i, value: el };
         })
-        
         // sorting the mapped array containing the reduced values
         mapped.sort(function(a, b) {
             if (a.value > b.value) {
@@ -32,7 +65,6 @@ class FundingRounds extends React.Component {
             }
             return 0;
         });
-        
         // container for the resulting order
         var result = mapped.map(function(el){
             return list[el.index];
@@ -41,7 +73,6 @@ class FundingRounds extends React.Component {
         return(
             <div>
                 {result}
-               
             </div>
         )
     }
@@ -50,3 +81,9 @@ class FundingRounds extends React.Component {
 export default FundingRounds;
 
 // Sorting with map: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Sorting_with_map 
+// Next steps: 
+//     1. Look at showcase loops through JSON content. 
+//     2. How to iterate over objects in React: https://stackoverflow.com/questions/40950546/react-js-right-way-to-iterate-over-object-instead-of-object-entries/40950656 
+//          a. Try this with simple test object. 
+//          b. Then try this with the JSON object. 
+//          c. Then try adding sort to this loop.
