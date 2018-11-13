55d3aede4f6a1160f69e606e1cdac73cfa652afd
Christopher Dougherty <christophertdoc@gmail>
2018-11-12 21:16:40 -0500

    add tabs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import Largest from './components/Largest';
 import Newest from '../src/components/Newest';
 import TopCategories from '../src/components/TopCategories';
+import Tabs from '../src/components/Tabs';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Largest />
-                <Newest />
-                <TopCategories />
+            <div>
+                <div className="App">
+                    <Largest />
+                    <Newest />
+                    <TopCategories />
+                </div>
+                <Tabs>
+                    <div label="Gator">
+                        See ya later, <em>Alligator</em>!
+                    </div>
+                    <div label="Croc">
+                        After 'while, <em>Crocodile</em>!
+                    </div>
+                    <div label="Sarcosuchus">
+                        Nothing to see here, this tab is <em>extinct</em>!
+                    </div>
+                </Tabs>
             </div>
         );
     }
